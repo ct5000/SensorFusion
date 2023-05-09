aa5494316d28ee38d4784037569bf9afdf21b882
ct5000 <crthul@gmail.com>
2023-05-09 12:37:46 +0200

    More data added. TargetAIS data is in a seperat folder and will need further adjustments

--- DataVisualise.py
+++ DataVisualise.py
@@ -12,22 +12,36 @@ print(data.info())
 
 latitude = data["latitude"]
 longitude = data["longitude"]
-enc_data = data["enc_data"]
+enc_data = data["enc_data"] #Fixed
 time = data["timestamp"]
+
+target_data = data["target_data"] #Fixed
+img_data = data["image_data"] #Fixed
+lidar = data["lidar1"] #fixed
+
+wradar0 = data["wradar_group0_id0"] 
+wradar1 = data["wradar_group0_id1"] 
+print(type(wradar0))
 
 time_np = np.array(time.array)
 time_diff = time_np[1:] - time_np[:-1]
 print(np.mean(time_diff))
 
-'''
+
 i = 0
-for d in enc_data:
-    truePath = "./ENC_imgs" + d[16:]
-    i += 1
-    cmd = "scp " + truePath + " ./enc_data"
+for d in wradar1:
     
-    os.system(cmd)
-'''
+    if (not isinstance(d,float)):
+        truePath = "./" + d[17:]
+        i += 1
+        cmd = "scp " + truePath + " ./radar1"
+        #print(cmd)
+        os.system(cmd)
+        #if (i>5):
+        #    break
+        
+        
+
     
 
 plt.plot(longitude,latitude)
